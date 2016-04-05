b785e751db9bf127264c6d284d2e1dfa4afd0217
Alex Telischak <alextelischak@gmail.com>
2016-04-04 22:35:46 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RootComponent from './src/RootComponent';
+
+ReactDOM.render( <RootComponent />, document.getElementById( 'component' ) );
---
+++ src/RootComponent.jsx
@@ -0,0 +1,11 @@
+import './RootComponent.scss';
+
+import React, { Component, } from 'react';
+
+export class RootComponent extends Component {
+  render() {
+    return ( <div>Component</div> );
+  }
+}
+
+export default RootComponent;
